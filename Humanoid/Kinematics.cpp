9c13af0776802accfcda7b55bcbc7e8f65919e7b
ps10659 <ps10659@gmail.com>
2015-05-30 09:49:58 +0800

    2
    2

--- Humanoid/Kinematics.cpp
+++ Humanoid/Kinematics.cpp
@@ -1,32 +1,60 @@
 #include "Kinematics.h"
 
-Configurations::Configurations()
+Configurations::Configurations( Trajectories *trajectories )
 {
-	angle_trunk_yaw.resize(TotalTimeFrame);
-	angle_waist.resize(TotalTimeFrame);
+	angle_trunk_yaw			= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_waist				= VectorXd::Zero(trajectories->TotalTimeFrame);
 
-	angle_left_hip_yaw.resize(TotalTimeFrame);
-	angle_left_hip_roll.resize(TotalTimeFrame);
-	angle_left_hip_pitch.resize(TotalTimeFrame);
-	angle_legt_knee.resize(TotalTimeFrame);
-	angle_left_ankle_pitch.resize(TotalTimeFrame);
-	angle_left_ankle_roll.resize(TotalTimeFrame);
+	angle_left_hip_yaw		= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_left_hip_roll		= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_left_hip_pitch	= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_left_knee			= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_left_ankle_pitch	= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_left_ankle_roll	= VectorXd::Zero(trajectories->TotalTimeFrame);
 
-	angle_right_hip_yaw.resize(TotalTimeFrame);
-	angle_right_hip_roll.resize(TotalTimeFrame);
-	angle_right_hip_pitch.resize(TotalTimeFrame);
-	angle_right_knee.resize(TotalTimeFrame);
-	angle_right_ankle_pitch.resize(TotalTimeFrame);
-	angle_right_ankle_roll.resize(TotalTimeFrame);
+	angle_right_hip_yaw		= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_right_hip_roll	= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_right_hip_pitch	= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_right_knee		= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_right_ankle_pitch	= VectorXd::Zero(trajectories->TotalTimeFrame);
+	angle_right_ankle_roll	= VectorXd::Zero(trajectories->TotalTimeFrame);
+}
+ForKinMatrix:: ForKinMatrix(double a, double alpha, double d, VectorXd *theta)
+{
+	int length = theta->size();
+	VectorXd zeros_vec = VectorXd::Zero(theta->size());
+	VectorXd ones_vec  = VectorXd::Ones(theta->size());
+	VectorXd cos_theta_vec = CosVector(theta);
+	VectorXd sin_theta_vec = SinVector(theta);
+
+	m11 = cos_theta_vec;
+	m12 = -1 * sin_theta_vec;
+	m13 = zeros_vec;
+	m14 = a * ones_vec;
+
+	m21 = cos(alpha) * sin_theta_vec;
+	m22 = cos(alpha) * cos_theta_vec;
+	m23 = -1 * sin(alpha) * ones_vec;
+	m24 = -1 * d * sin(alpha) * ones_vec;
+
+	m31 = sin(alpha) * sin_theta_vec;
+	m32 = sin(alpha) * cos_theta_vec;
+	m33 = cos(alpha) * ones_vec;
+	m34 = d * cos(alpha) * ones_vec;
+
+	m41 = zeros_vec;
+	m42 = zeros_vec;
+	m43 = zeros_vec;
+	m44 = ones_vec;
 }
 
-string SetAngleTrunkYaw( Configurations *configurations )
+string DesignAngleTrunkYaw( Configurations *configurations )
 {
 	string err;
 	cout << "Calculating angle_trunk_yaw trajectory..." << endl;
 
 	// temp acceptable solution for angle_trunk_yaw 
-		configurations->angle_trunk_yaw.setZero();
+	configurations->angle_trunk_yaw.setZero();
 
 	// angle_trunk_yaw trajectory legal test
 	if(0){
@@ -39,7 +67,7 @@ string SetAngleTrunkYaw( Configurations *configurations )
 
 	return err;
 }
-string SetAngleWaist( Configurations *configurations )
+string DesignAngleWaist( Configurations *configurations )
 {
 	string err;
 	cout << "Calculating angle_waist trajectory..." << endl;
@@ -60,14 +88,17 @@ string SetAngleWaist( Configurations *configurations )
 }
 string LowerBodyInvKin( Configurations *configurations, Trajectories *trajectories )
 {
-	// TODO: Given com_direction, foot_direction
+	// TODO: Given com_direction, foot_direction, angle_trunk_yaw, angle_waist
 	//		 Calculate all lower body angle
 	string err;
-	cout << "Calculating angle_waist trajectory..." << endl;
+	cout << "Calculating all lower body angle trajectory..." << endl;
 
-	// temp acceptable solution for angle_waist 
+	CalAngleHipYaw( configurations, trajectories );
 
-
+	ForKinMatrix T_oa_left = ForKinMatrix(0, 0, 0, &(trajectories->com_direction));
+	ForKinMatrix T_ab_left = ForKinMatrix(0, 0, 0, &(configurations->angle_trunk_yaw));
+	ForKinMatrix T_bc_left = ForKinMatrix(0, 0, 0, &(configurations->angle_waist));
+	ForKinMatrix T_cd_left = ForKinMatrix(0, 0, 0, &(configurations->angle_left_hip_yaw));
 
 
 
@@ -84,3 +115,34 @@ string LowerBodyInvKin( Configurations *configurations, Trajectories *trajectori
 
 
 }
+void CalAngleHipYaw( Configurations *configurations, Trajectories *trajectories )
+{
+	configurations->angle_left_hip_yaw = trajectories->com_direction - trajectories->left_foot_direction - configurations->angle_trunk_yaw;
+	configurations->angle_right_hip_yaw = trajectories->com_direction - trajectories->right_foot_direction - configurations->angle_trunk_yaw;
+
+}
+//VectorXd VectorBitwiseMultiply( VectorXd *v1, VectorXd *v2)
+//{
+//	VectorXd v(v1->size());
+//	for(int i=0; i<v1->size(); i++)
+//		v[i] = v1[i]*v2[i];
+//
+//	return v;
+//}
+VectorXd CosVector( VectorXd *theta)
+{
+	VectorXd m(theta->size());
+	for(int i=0; i<theta->size(); i++)
+		m[i] = cos((*theta)[i]);
+
+	return m;
+}
+VectorXd SinVector( VectorXd *theta)
+{
+	VectorXd m(theta->size());
+	for(int i=0; i<theta->size(); i++)
+		m[i] = sin((*theta)[i]);
+
+	return m;
+}
+
